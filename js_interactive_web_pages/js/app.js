4e7fb6783030812f967cc6e37ef20b477d6a9a9f
Ariel Kaplan <ariel.h.kaplan@gmail.com>
2015-11-08 23:49:33 -0800

    Add pseudocode plan

---
+++ js_interactive_web_pages/js/app.js
@@ -0,0 +1,22 @@
+// Add new task
+// When click next sibling button to #new-task input
+// save value of #new-task
+// append value as li to #incomplete-tasks ul, with additional label/buttons
+
+
+// Edit existing task
+// On click .edit button
+// Add .editMode class to parent li
+// button.edit text changes to "Save"
+
+
+// Delete existing task
+// On click .delete button
+// Alert confirmation
+// Remove parent li
+
+
+// Mark task as complete/incomplete
+// On checkbox change
+// if in #incomplete-tasks, move to #completed-tasks
+// if in #complete-tasks, move to #incomplete-tasks
